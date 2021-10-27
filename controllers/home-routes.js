46e1e2d431e4060dd02178ceb4a9d552d2099fac
D Kornbluth <dwight.kornbluth@gmail.com>
2021-10-27 16:27:46 -0400

    updated user-routes and home-routes js files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,9 +54,17 @@ router.get('/reviews/:id', withAuth, async (req, res) => {
             loggedIn: req.session.loggedIn,
         });
     } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+        console.log(err);
+        res.status(500).json(err);
     }
-  });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+})
 
 module.exports = router;
