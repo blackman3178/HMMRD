ad7e24e5b0b4b8cdae07718edc1b26989184a415
D Kornbluth <dwight.kornbluth@gmail.com>
2021-10-27 12:11:46 -0400

    pushing changes to controller/index.js and home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,70 +1,61 @@
 const router = require('express').Router();
 const { Comment, Review } = require('../../HMMRD/models');
+const withAuth = require('../utils/auth');
 
 // GET all drink reviews for homepage
-router.get('/', async (req, res) => {
-  try {
-    const dbDrinkData = await Drinks.findAll({
-      include: [
-        {
-          model: apiData,
-          attributes: ['image_source', 'description'],
-        },
-      ],
-    });
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const dbDrinkData = await Review.findAll({
+        include: [
+            {
+            model: Review,
+            attributes: ['drink_name', 'image', 'review'],
+            },
+        ],
+        });
 
-    const drinks = dbDrinkData.map((drink) =>
-      drink.get({ plain: true })
-    );
+        const drinks = dbDrinkData.map((drink) =>
+        drink.get({ plain: true })
+        );
 
-    res.render('homepage', {
-      drinks,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+        res.status(200).render('homepage', {
+            drinks,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
-// GET one gallery
-router.get('/gallery/:id', async (req, res) => {
-  try {
-    const dbGalleryData = await Gallery.findByPk(req.params.id, {
-      include: [
-        {
-          model: Painting,
-          attributes: [
-            'id',
-            'title',
-            'artist',
-            'exhibition_date',
-            'filename',
-            'description',
-          ],
-        },
-      ],
-    });
-
-    const gallery = dbGalleryData.get({ plain: true });
-    res.render('gallery', { gallery });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
-// GET one painting
-router.get('/painting/:id', async (req, res) => {
-  try {
-    const dbPaintingData = await Painting.findByPk(req.params.id);
-
-    const painting = dbPaintingData.get({ plain: true });
-
-    res.render('painting', { painting });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
+// GET one drink review for homepage
+router.get('/reviews/:id', withAuth, async (req, res) => {
+    try {
+        const dbDrinkData = await Review.findByPk(req.params.id, {
+        include: [
+            {
+            model: Comment,
+            attributes: [
+                'id',
+                'popularity',
+                'user_id',
+                'drink_id',
+                'comment',
+            ],
+            },
+        ],
+        });
+  
+        const drinkReviews = dbDrinkData.get({ plain: true });
+  
+        res.status(200).render('ctreviews-details', {
+            drinkReviews,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
