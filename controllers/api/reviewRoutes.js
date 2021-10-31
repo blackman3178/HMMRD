507d0d58639ab50bdffebc3a78431108f6a0edfe
Noah Blackman <nablackm@usc.edu>
2021-10-31 12:22:50 -0400

    stash

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -5,10 +5,12 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     const body = req.body;
+    console.log(body);
 
     try {
         const newPost = await Review.create({ ...body, user_id: req.session.user_id });
         res.json(newPost);
+        console.log(newPost);
     } catch (err) {
         res.status(500).json(err);
     }
