ed477851c0bfd8b629736f3d2596fb89a6487d61
Noah Blackman <nablackm@usc.edu>
2021-10-31 16:57:27 -0400

    Some final touches

--- models/Review.js
+++ models/Review.js
@@ -1,6 +1,7 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require('../config/connection');
+const axios = require('axios').default;
 
 class Review extends Model {
     checkPassword(loginPw) {
@@ -42,13 +43,48 @@ Review.init(
         },
     },
     {
-        hooks: { //TODO: tried to use hooks to create logic to make review 0 or not!!!
-            beforeCreate: async (newReviewData)=> {
-                if(newReviewData.popularity !== (0)){
+        hooks: {
+            beforeCreate: async (newReviewData) => {
+                if (newReviewData.popularity !== (0)) {
                     newReviewData.popularity = 0;
                     return newReviewData;
-                } 
+                }
             },
+            beforeCreate: async (newReviewData) => {
+                //future development
+                function getDrinkImage(drinkName) {
+                    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+                    axios.get(url + drinkName)
+                        .then(function (response) {
+                            //console.log(response);
+                            if (response.data.drinks !== null) {
+                                const data = response.data.drinks.map((drink, index) => {
+                                    if (index === 0) {
+                                        const newDrink = {
+                                            name: drink.strDrink,
+                                            img: drink.strDrinkThumb
+                                        };
+                                        console.log(drink.strDrinkThumb);
+                                        return "../../public/images/hmmrdlight.png";
+                                    }
+                                });
+                            } else {
+                                const newDrink = {
+                                    img: "../../public/images/hmmrdlight.png"
+                                };
+                                return newDrink;
+                            }
+
+                        }).catch(function (error) {
+                            console.log(error);
+                        })
+                }
+                if (newReviewData.id <= 10) {
+                    const drinkFromApi = getDrinkImage(newReviewData.drink_name);
+                    // const drinkImage = drinkFromApi.img;
+                    newReviewData.image = drinkFromApi;
+                }
+            }
         },
         sequelize,
         timestamps: false,
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require("./utils/auth");
-
+const drinkImages = require("./controllers/api/apiRoutes");
 const sequelize = require('./config/connection');
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
