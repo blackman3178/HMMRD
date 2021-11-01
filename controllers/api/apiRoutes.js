0aa7c6a9cfd5a282ada9a88fed62511b2c2ff713
Tammy Gagliano <tracsd@gmail.com>
2021-10-31 20:55:41 -0500

    update readme and add screenshot

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -38,32 +38,3 @@ router.get('/drink/:drinkName', (req, res) => {
 module.exports = router;
 
 //future development
-// function getDrinkImage(drinkName){
-//     const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-//     axios.get(url + drinkName)
-//     .then(function (response) {
-//         //console.log(response);
-//     if (response.data.drinks !== null){
-//         const data = response.data.drinks.map((drink,index) => {
-//             if (index === 0){
-//                 const newDrink = {
-//                     name: drink.strDrink,
-//                     img: drink.strDrinkThumb
-//                    };
-//                    console.log(drink.strDrinkThumb);
-//                    return "../../public/images/hmmrdlight.png";
-//             }
-//         });   
-//     }else {
-//         const newDrink = {
-//             img:"../../public/images/hmmrdlight.png"
-//         };
-//         return newDrink;
-//     }
-
-//     }).catch(function(error) {
-//         console.log(error);
-//     })
-// }
-
-// module.exports = { getDrinkImage} ;
